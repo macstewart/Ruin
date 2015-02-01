e75478794f797882ba040b8615af368499fbd139
90ne1 <mac.stewart@live.ca>
2015-02-01 14:57:28 -0500

    Moved admin controls from camera to blank gameobject, and added singleton behavior to avoid multiple versions being created

---
+++ Assets/Scripts/SceneControls.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneControls : MonoBehaviour {
+
+	SceneControls controller;
+	
+	void Awake () {
+		if (controller == null) {
+			DontDestroyOnLoad(gameObject);
+			controller = this;
+		} else if (controller != this) {
+			Destroy(this);
+		}
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Application.LoadLevel (Application.loadedLevelName);
+		}
+		if (Input.GetKeyDown (KeyCode.P)) {
+			if (Time.timeScale != 0){
+				Time.timeScale = 0;
+			} else {
+				Time.timeScale = 1;
+			}
+		}
+	
+	}
+}
