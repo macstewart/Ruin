1fb36dab91d195da43a3f30efa8a210f62f52fcd
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2015-02-08 14:10:17 -0500

    Remove target GameObject script. For player interaction

---
+++ Assets/Scripts/RemoveBlockage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveBlockage : MonoBehaviour {
+
+	public GameObject target;
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
+			Destroy(target);
+
+		}
+		
+	}
+}
