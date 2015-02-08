6651383e901a1f8684d745e0097e4609cf70dc8d
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2015-02-08 14:38:18 -0500

    Script changes: disable renderer and collider on blockage for a specific amount of time

--- Assets/Scripts/RemoveBlockage.cs
+++ Assets/Scripts/RemoveBlockage.cs
@@ -4,12 +4,29 @@ using System.Collections;
 public class RemoveBlockage : MonoBehaviour {
 
 	public GameObject target;
+	float timeSinceLastCollision = 0;
+	public float delayTime;
+	bool collision = false;
 
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
-			Destroy(target);
+			target.renderer.enabled = false;
+			target.collider2D.enabled = false;
+			collision = true;
 
 		}
-		
+	}
+
+	void Update (){
+
+		if (collision) {
+			Debug.Log ("hit");
+			timeSinceLastCollision += Time.deltaTime;  
+			if (timeSinceLastCollision > delayTime) {
+					target.renderer.enabled = true;
+					target.collider2D.enabled = true;
+					timeSinceLastCollision = 0;
+			}
+		}
 	}
 }
