5c3e991abdbccbba22742c628d51bc87c426b20f
90ne1 <mac.stewart@live.ca>
2015-02-10 15:37:24 -0500

    Added pressure plate prefab for toggling obstacles

---
+++ Assets/Scripts/MovingPlatformController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatformController : MonoBehaviour {
+
+	public int blockRange = 5;
+	public float speed = 2f;
+	public float returnDelay;
+	public bool vertical;
+	public bool flipMovement;
+
+	const float BLOCK_WIDTH = 0.32f;
+	float currentSpeed;
+	float range;
+
+	void Awake () {
+		range = blockRange*BLOCK_WIDTH;
+		if (flipMovement)
+			currentSpeed = -speed;
+		else 
+			currentSpeed = speed;
+
+
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (!vertical) {
+		} else {
+
+		}
+	
+	}
+}
--- Assets/Scripts/PressurePlate.cs
+++ Assets/Scripts/PressurePlate.cs
@@ -1,30 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RemoveBlockage : MonoBehaviour {
+public class PressurePlate : MonoBehaviour {
 
 	public GameObject target;
 	float timeSinceLastCollision = 0;
 	public float delayTime;
 	bool collision = false;
-
+	
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
+			transform.localScale = new Vector3(1f, -1.5f, 1f);
 			target.renderer.enabled = false;
 			target.collider2D.enabled = false;
 			collision = true;
-
+			timeSinceLastCollision = 0;
 		}
 	}
-	void Update (){
 
-		if (collision) {
+	void OnCollisionExit2D(Collision2D col) {
+		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
+			collision = false;
+		}
+
+	}
+	
+
+	void Update (){
+		
+		if (!collision) {
 			timeSinceLastCollision += Time.deltaTime;  
 			if (timeSinceLastCollision > delayTime) {
-					target.renderer.enabled = true;
-					target.collider2D.enabled = true;
-					timeSinceLastCollision = 0;
+				transform.localScale = new Vector3(1f, -1f, 1f);
+				target.renderer.enabled = true;
+				target.collider2D.enabled = true;
+				timeSinceLastCollision = 0;
 			}
 		}
+
 	}
 }
