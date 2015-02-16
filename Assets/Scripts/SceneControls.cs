34dd5b534e765d7d65bd234e425ddf4e96b419df
90ne1 <mac.stewart@live.ca>
2015-02-16 15:49:44 -0500

    Skeleton pause menu with reset button

--- Assets/Scripts/SceneControls.cs
+++ Assets/Scripts/SceneControls.cs
@@ -5,6 +5,7 @@ public class SceneControls : MonoBehaviour {
 
 	public static SceneControls controller;
 	public bool paused = false;
+	GameObject pauseMenu;
 	
 	void Awake () {
 		if (controller == null) {
@@ -13,22 +14,46 @@ public class SceneControls : MonoBehaviour {
 		} else if (controller != this) {
 			Destroy(this);
 		}
-	}
 
+	}
+	void Start () {
+		GetPauseMenu();
+	}
+	
 	void Update () {
 
+		if (pauseMenu == null) {
+			GetPauseMenu();
+		}
+
 		if (Input.GetKeyDown (KeyCode.Escape)) {
-			Application.LoadLevel (Application.loadedLevelName);
+			ResetScene();
 		}
 		if (Input.GetKeyDown (KeyCode.P)) {
 			if (!paused){
 				Time.timeScale = 0;
 				paused = true;
+				pauseMenu.SetActive(true);
 			} else {
 				Time.timeScale = 1;
 				paused = false;
+				pauseMenu.SetActive(false);
 			}
 		}
 	
+	}
+
+	void GetPauseMenu() {
+		pauseMenu = GameObject.Find ("PauseMenu");
+		pauseMenu.SetActive(false);
+	}
+
+	public void ResetScene() {
+		Application.LoadLevel (Application.loadedLevelName);
+		if (paused) {
+			Time.timeScale = 1;
+			paused = false;
+			pauseMenu.SetActive(false);
+		}
 	}
 }
