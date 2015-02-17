f611ce57d173b190bafeb0fa1e1cf78e45797256
90ne1 <mac.stewart@live.ca>
2015-02-16 22:37:50 -0500

    Collision management tweaks

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -4,10 +4,10 @@ using System.Collections;
 public class CollisionDetection : MonoBehaviour {
 
 
-	// Use this for initialization
+	// Resets the current scene if the right or left player comes in contact with this object (use for simple hazards)
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
-			Application.LoadLevel("test");
+			Application.LoadLevel(Application.loadedLevelName);
 
 		}
 		
--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -7,24 +7,24 @@ public enum PlatformType{
 
 public class PlatformManager : MonoBehaviour {
 
-	public PlatformType type = PlatformType.wall;
-	public bool textureOverride = false;
-	public bool rightSideEdge = false;
-	public BoxCollider2D edge, innerEdge, floor, wall;
-	SpriteRenderer spriteRend;
-	Sprite[] sprites;
+	public PlatformType type = PlatformType.wall;	//Platform type enum used to texture and place hitboxes. Defaults to wall (no friction boxes)
+	public bool textureOverride = false;	//Toggle that allows dev to set custom texture for individual blocks
+	public bool rightSideEdge = false;		//Set whether platform is on the left or right side. Flips texture and hitboxes if true
+	public BoxCollider2D edge, innerEdge, floor, wall;	//Self referencing box colliders. For toggling indivual colliders
+	SpriteRenderer spriteRend;		//Self referencing. Used to change sprite(texture) on the fly
+	Sprite[] sprites;	//Container for sprites to allow loading other textures on the fly
 
 	// Use this for initialization
 	void Awake () {
 		sprites = Resources.LoadAll<Sprite>("Sprites/Platforms/goodly-2x");
 		spriteRend = GetComponent<SpriteRenderer>();
-		switch (type){
-		case PlatformType.middle: 
+		switch (type){				
+		case PlatformType.middle: //If middle platform, load middle texture and set full friction hitbox
 			if (!textureOverride)
 				spriteRend.sprite = sprites[5];
 			floor.enabled = true;
 			break;
-		case PlatformType.edge:
+		case PlatformType.edge:		//If edge, set no friction outer side and full friction top, flip texture if necessary
 			if (!textureOverride)
 				spriteRend.sprite = sprites[4];
 			edge.enabled = true;
@@ -32,7 +32,7 @@ public class PlatformManager : MonoBehaviour {
 			if (rightSideEdge)
 				transform.localScale = new Vector3(transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
 			break;
-		case PlatformType.wall:
+		case PlatformType.wall:		//If wall, set no friction hitbox. Flip texture if necessary
 			if (!textureOverride)
 				spriteRend.sprite = sprites[7];
 			if (rightSideEdge)
