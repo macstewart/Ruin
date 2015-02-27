047a7169f9acd39dfd5be1bf36e6c84975309078
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2015-02-27 17:53:36 -0500

    CollisionDetection script altered so on collision with hazard the players are respawned and the scene is not reset. This is required so the Timer continues until both players reach the portal Timer script created.

--- Assets/Scripts/CollisionDetection.cs
+++ Assets/Scripts/CollisionDetection.cs
@@ -2,14 +2,33 @@
 using System.Collections;
 
 public class CollisionDetection : MonoBehaviour {
+	
 
-
+	private Vector3 startPosRight;
+	private Vector3 startPosLeft;
+	
+	void Start(){
+		startPosRight = GameObject.Find("RightPlayer").transform.position;
+		startPosLeft = GameObject.Find("LeftPlayer").transform.position;
+	}
+	
+	
 	// Resets the current scene if the right or left player comes in contact with this object (use for simple hazards)
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
-			Application.LoadLevel(Application.loadedLevelName);
+			//Application.LoadLevel(Application.loadedLevelName);
 
+			respawn();
+			
 		}
+		
+	}
+	
+	void respawn(){
+		
+		GameObject.Find("RightPlayer").transform.position = startPosRight;
+		GameObject.Find("LeftPlayer").transform.position = startPosLeft;
+		
 		
 	}
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	private float startTime;
+	private float newTime;
+
+	void Awake(){
+		startTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		newTime = Time.time - startTime;
+		//Debug.Log(newTime);
+	}
+}
