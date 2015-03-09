b88eb2169528b02fff6eab90a89785f37252de3a
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2015-03-09 15:12:54 -0400

    Timer display on screen to 1 significant digit. Pause menu also works with timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,27 +2,31 @@
 using System.Collections;
 
 public class Timer : MonoBehaviour {
-
-	private float startTime;
-	private float newTime;
+	
+	private float newTime=0;
 	private float endTime;
 
 	public bool leftPlayerCompletion = false;
 	public bool rightPlayerCompletion = false;
 	
 
-	void Awake(){
-		startTime = Time.time;
+	void Start(){
+		InvokeRepeating( "IncreaseTime", 0.1f, 0.1f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		newTime = Time.time - startTime;
-		//Debug.Log(newTime);
+
+
+	void IncreaseTime(){
+		newTime = Mathf.Round((newTime + 0.1f)*100.0f) / 100.0f;
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(10,10,400,90), "Timer = " + newTime.ToString () + " Seconds");
+
 	}
 
 	public void EndTimer(){
-		endTime =Time.time -  startTime;
+		endTime = Mathf.Round(newTime *100.0f) / 100.0f;;
+		CancelInvoke (); //cancles invokeRepeating
 		Debug.Log (endTime);
 	}
 }
