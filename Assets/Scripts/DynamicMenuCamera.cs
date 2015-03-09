22ebcf5e56da1cb4ffa0ce36e11e051ef14e2f78
90ne1 <mac.stewart@live.ca>
2015-03-09 11:13:11 -0400

    Revert "Revert "Initial start menu graphic design (recommit)""
    This reverts commit 43b5dea4f653121fa01b2696237f664ec3e66307.

--- Assets/Scripts/DynamicMenuCamera.cs
+++ Assets/Scripts/DynamicMenuCamera.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class DynamicMenuCamera : MonoBehaviour {
 
+	public GameObject background;
 	Rect screenRect;
 	
 	// Use this for initialization
@@ -14,5 +15,6 @@ public class DynamicMenuCamera : MonoBehaviour {
 	void Update () {
 		if (screenRect.Contains(Input.mousePosition))
 			transform.position = new Vector3((Input.mousePosition.x - Screen.width/2)/500, (Input.mousePosition.y - Screen.height/2)/500, transform.position.z);
+			background.transform.position = new Vector3(transform.position.x*0.8f, transform.position.y*0.8f, background.transform.position.z);
 	}
 }
