b17fa14a044363ba1cb709195e0e49c690f3c50d
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2015-03-17 12:44:26 -0400

    tutorial level 4 design and addition modified pressure plate script created

---
+++ Assets/Scripts/PressurePlate2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate2 : MonoBehaviour {
+	
+	public GameObject target;
+	public GameObject target2;
+	public float delayTime;
+	
+	private float timeSinceLastCollision = 0;
+	private bool collision = false;
+	
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
+			this.renderer.enabled = false;
+			target.SetActive(false);
+			target2.SetActive (false);
+			collision = true;
+			timeSinceLastCollision = 0;
+		}
+	}
+	
+	
+	
+	void OnTriggerExit2D(Collider2D col) {
+		if(col.gameObject.name=="RightPlayer" | col.gameObject.name=="LeftPlayer"){
+			collision = false;
+			
+		}
+	}
+	
+	
+	void Update (){
+		if (!collision) {
+			timeSinceLastCollision += Time.deltaTime;  
+			if (timeSinceLastCollision > delayTime) {
+				this.renderer.enabled = true;
+				target.SetActive(true);
+				target2.SetActive(true);
+				timeSinceLastCollision = 0;
+			}
+		}
+		
+	}
+}
