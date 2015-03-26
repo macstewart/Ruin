3e7a6b8ae7ec74688db073456a35561a821e2f14
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2015-03-26 11:05:54 -0400

    Fade Text addition to Tutorial Level One Tutorial completion menu colour fix to all levels

---
+++ Assets/Scripts/EntryText.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EntryText : MonoBehaviour {
+
+	public static EntryText controller;
+
+	float fadeSpeed = 0.5f;
+	Text text;
+	Color color;
+
+	// Use this for initialization
+	void Start () {
+
+		Text[] textList =this.GetComponentsInChildren<Text> ();
+		text = textList [0];
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//float newColor = text.color.a - 0.1f;
+		//text.color.a = newColor;
+
+		color.a = text.color.a - 0.01f;
+		text.color = color;
+
+	
+	}
+
+}
